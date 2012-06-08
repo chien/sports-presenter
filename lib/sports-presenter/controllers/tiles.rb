50818f45a98b0818dce03c9c7f88fe6343173e84
Graeme Wilson <graeme.wilson@iplayup.com>
2012-06-08 12:16:20 +1000

    adding section_type as part of the usage

--- lib/sports-presenter/controllers/tiles.rb
+++ lib/sports-presenter/controllers/tiles.rb
@@ -3,7 +3,8 @@ module SportsPresentation
     
     get "/tiles" do
       content_type :json
-      tile_data = TileProvider.fetch_tiles(params[:subject_url]).generate(:locale => params[:locale], :region => params[:region])
+      context_url = !params[:subject_url].blank? ? params[:subject_url] : params[:section_type]
+      tile_data = TileProvider.fetch_tiles(context_url).generate(:locale => params[:locale], :region => params[:region])
       tile_data.to_json
     end
     
