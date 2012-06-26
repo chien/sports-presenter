4bf7f9a8f9d7c3e87c449f044d979dc7fb921ab3
Graeme Wilson <graeme.wilson@iplayup.com>
2012-06-26 18:51:32 +1000

    try without Airbreak

--- lib/sports-presenter/controllers/tiles.rb
+++ lib/sports-presenter/controllers/tiles.rb
@@ -3,11 +3,6 @@ module SportsPresentation
     set :raise_errors, true
     set :show_exceptions, true
 
-    Airbrake.configure do |config|
-      config.api_key = '757646976907fb3ae7930559b64cbe58'
-    end
-    use Airbrake::Rack
-
     error do
       content_type :json
       status 400 # or whatever
@@ -15,7 +10,7 @@ module SportsPresentation
       e = env['sinatra.error']
       {:result => 'error', :message => e.message}.to_json
     end
-    
+
     get "/tiles" do
       content_type :json
       context_url = params[:subject_url].to_s.length != 0 ? params[:subject_url] : params[:section_type]
