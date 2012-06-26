2adc322824a2ab81fa535164dd23355f50334960
Graeme Wilson <graeme.wilson@iplayup.com>
2012-06-26 18:39:06 +1000

    moving the target

--- lib/sports_presenter.rb
+++ lib/sports_presenter.rb
@@ -43,21 +43,6 @@ I18n.load_path += Dir[File.join(here, "sports-presenter", "i18n", "*.yml")]
 
 module SportsPresentation
   class Application < Sinatra::Base
-    set :raise_errors, true
-    set :show_exceptions, true
-
-    Airbrake.configure do |config|
-      config.api_key = '757646976907fb3ae7930559b64cbe58'
-    end
-    use Airbrake::Rack
-
-    error do
-      content_type :json
-      status 400 # or whatever
-
-      e = env['sinatra.error']
-      {:result => 'error', :message => e.message}.to_json
-    end
 
     use LocaleRequest
     use CacheSettings, {
