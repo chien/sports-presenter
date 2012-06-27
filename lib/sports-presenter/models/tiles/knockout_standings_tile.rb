77a431a64bd706b1aa08c81eefa4706b300cb425
Graeme Wilson <graeme.wilson@iplayup.com>
2012-06-27 15:02:55 +1000

    changed the tile for knock out standings

--- lib/sports-presenter/models/tiles/knockout_standings_tile.rb
+++ lib/sports-presenter/models/tiles/knockout_standings_tile.rb
@@ -3,9 +3,9 @@ module SportsPresentation
     class KnockoutStandingsTile < SolidTileDisplay
       def initialize
         super title: :knockout_standings, background_image: {
-          low: "/tiles/standings/football/low_148_88.png",
-          medium: "/tiles/standings/football/medium_222_132.png",
-          high: "/tiles/standings/football/high_296_176.png"          
+          low: "/tiles/knockout-standings/football/low_148_88.png",
+          medium: "/tiles/knockout-standings/football/medium_222_132.png",
+          high: "/tiles/knockout-standings/football/high_296_176.png"          
         }, background_color: "#8CB5B8"
       end
     end
