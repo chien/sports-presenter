0938704401b83afab7f96f0c0b8e7e867e904786
Graeme Wilson <graeme.wilson@iplayup.com>
2012-06-29 12:00:57 +1000

    correct mime types for groups

--- lib/sports-presenter/models/tiles/grouping_tile.rb
+++ lib/sports-presenter/models/tiles/grouping_tile.rb
@@ -5,7 +5,7 @@ module SportsPresentation
       def initialize(name, uid)
         @uid = uid
         @name = name
-        @link_type = "vnd.playup.grouping"
+        @link_type = "application/vnd.playup.grouping+json"
         super title: name, background_image: {
           low: "/tiles/standings/football/low_148_88.png",
           medium: "/tiles/standings/football/medium_222_132.png",
